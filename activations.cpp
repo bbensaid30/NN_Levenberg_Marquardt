5816c88032fd3976d53fc5e951fdb7ebcb87273e
Bilel BENSAID <bilel.bensaid@etu.emse.fr>
2021-04-26 20:40:52 +0200

    ajout des activations pour les tests

--- activations.cpp
+++ activations.cpp
@@ -1,5 +1,10 @@
 #include "activations.h"
 
+void linear(Eigen::MatrixXd& Z, Eigen::MatrixXd& S)
+{
+    S = Eigen::MatrixXd::Constant(Z.rows(),Z.cols(),1);
+}
+
 void sigmoid(Eigen::MatrixXd& Z, Eigen::MatrixXd& S)
 {
     Z = (1+(-Z).array().exp()).inverse();
@@ -20,11 +25,16 @@ void reLU(Eigen::MatrixXd& Z, Eigen::MatrixXd& S)
     S = (Z.array()>0).select(U,0);
 }
 
+
 void activation(std::string nameActivation, Eigen::MatrixXd& Z, Eigen::MatrixXd& S)
 {
     if (nameActivation == "sigmoid")
     {
         sigmoid(Z,S);
+    }
+    else if (nameActivation == "linear")
+    {
+        linear(Z,S);
     }
     else if(nameActivation == "tanh")
     {
@@ -34,14 +44,75 @@ void activation(std::string nameActivation, Eigen::MatrixXd& Z, Eigen::MatrixXd&
     {
         reLU(Z,S);
     }
+    else if(nameActivation == "polyTwo")
+    {
+        polyTwo(Z,S);
+    }
+    else if(nameActivation=="polyThree")
+    {
+        polyThree(Z,S);
+    }
+    else if(nameActivation=="polyFour")
+    {
+        polyFour(Z,S);
+    }
+    else if(nameActivation=="expTwo")
+    {
+        expTwo(Z,S);
+    }
+    else if(nameActivation=="expFour")
+    {
+        expFour(Z,S);
+    }
+    else if(nameActivation=="expFive")
+    {
+        expFive(Z,S);
+    }
     else
     {
-        sigmoid(Z,S);
+        linear(Z,S);
     }
 }
 
 
+void polyTwo(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c)
+{
+    S = 2*std::exp(c/2)*Z.array();
+    Z = std::exp(c/2)*(Z.array().pow(2)-1);
+}
 
+void polyThree(Eigen::MatrixXd& Z, Eigen::MatrixXd& S, double c)
+{
+    S = 6*std::exp(c/2)*Z.array()*(Z.array()-1);
+    Z = std::exp(c/2)*(2*Z.array().pow(3)-3*Z.array().pow(2)+5);
+}
+
+void polyFour(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c)
+{
+    S = 4*std::exp(c/2)*Z.array()*(Z.array().pow(2)-1);
+    Z = std::exp(c/2)*(Z.array().pow(4)-2*Z.array().pow(2)+3);
+}
+
+void expTwo(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c)
+{
+    Eigen::MatrixXd inter = (1/2*(Z.array().pow(2)-2*Z.array().pow(2)+c)).exp();
+    S = (Z.array()-1)*inter.array();
+    Z = inter;
+}
+
+void expFour(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c)
+{
+    Eigen::MatrixXd inter = (1/2*(Z.array().pow(4)-2*Z.array().pow(2)+c)).exp();
+    S = 2*Z.array()*(Z.array().pow(2)-1)*inter.array();
+    Z = inter;
+}
+
+void expFive(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c)
+{
+    Eigen::MatrixXd inter = (1/2*(6*Z.array().pow(5)-15*Z.array().pow(4)-10*Z.array().pow(3)+30*Z.array().pow(2)+c)).exp();
+    S = 15*Z.array()*(Z.array().pow(3)-2*Z.array().pow(2)-Z.array()+2)*inter.array();
+    Z = inter;
+}
 
 
 
--- activations.h
+++ activations.h
@@ -5,9 +5,20 @@
 #include <cmath>
 #include <Eigen/Dense>
 
+void linear(Eigen::MatrixXd& Z, Eigen::MatrixXd& S);
 void sigmoid(Eigen::MatrixXd& Z, Eigen::MatrixXd& S);
 void tanh(Eigen::MatrixXd& Z, Eigen::MatrixXd& S);
 void reLU(Eigen::MatrixXd& Z, Eigen::MatrixXd& S);
+
+
+
+void polyTwo(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c=0);
+void polyThree(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c=0);
+void polyFour(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c=0);
+void expTwo(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c=0);
+void expFour(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c=0);
+void expFive(Eigen::MatrixXd& Z, Eigen::MatrixXd& S,double c=0);
+
 
 void activation(std::string nameActivation, Eigen::MatrixXd& Z, Eigen::MatrixXd& S);
 
