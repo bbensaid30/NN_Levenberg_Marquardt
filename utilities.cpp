9541d79b3e441a752d8eb30b2fbfd563deb7c8bd
Bilel BENSAID <bilel.bensaid@etu.emse.fr>
2021-05-25 00:16:50 +0200

    fonctions pour vectoriser les poids et pour tester si on a trouvé un nouveau point

--- utilities.cpp
+++ utilities.cpp
@@ -19,3 +19,86 @@ int proportion(Eigen::VectorXd const& currentPoint, std::vector<Eigen::VectorXd>
     }
     return -1;
 }
+
+double distance(std::vector<Eigen::MatrixXd> const& weightsPrec, std::vector<Eigen::VectorXd> const& biasPrec,
+std::vector<Eigen::MatrixXd> const& weights, std::vector<Eigen::VectorXd> const& bias, std::string norm)
+{
+    double dis=0.0, disCurrent;
+    int const L = weights.size();
+    int l;
+
+    if(norm=="infinity")
+    {
+        for(l=0;l<L;l++)
+        {
+            disCurrent = (weights[l]-weightsPrec[l]).lpNorm<Eigen::Infinity>();
+            if(disCurrent>dis){dis=disCurrent;}
+            disCurrent = (bias[l]-biasPrec[l]).lpNorm<Eigen::Infinity>();
+            if(disCurrent>dis){dis=disCurrent;}
+        }
+        return dis;
+    }
+    else
+    {
+        for(l=0;l<L;l++)
+        {
+            dis += (weights[l]-weightsPrec[l]).squaredNorm();
+            dis += (bias[l]-biasPrec[l]).squaredNorm();
+        }
+        return std::sqrt(dis);
+    }
+
+}
+
+double cosVector(Eigen::VectorXd const& v1, Eigen::VectorXd const& v2)
+{
+    double result;
+    result = v1.dot(v2);
+    return result/=v1.norm()*v2.norm();
+}
+
+void tabToVector(std::vector<Eigen::MatrixXd>& weights, std::vector<Eigen::VectorXd> const& bias, int const& L, std::vector<int> const& nbNeurons, std::vector<int> const& globalIndices,
+Eigen::VectorXd& point)
+{
+    int l, jump;
+
+    jump=nbNeurons[L]*nbNeurons[L-1];
+    weights[L].resize(jump,1);
+    point.segment(globalIndices[2*(L-1)]-jump,jump)=weights[L-1];
+    jump=nbNeurons[L];
+    point.segment(globalIndices[2*(L-1)+1]-jump,jump)=bias[L-1];
+    for (l=L-1;l>0;l--)
+    {
+        jump=nbNeurons[l]*nbNeurons[l-1];
+        weights[l-1].resize(jump,1);
+        point.segment(globalIndices[2*(l-1)]-jump,jump)=weights[l-1];
+        jump=nbNeurons[l];
+        point.segment(globalIndices[2*(l-1)+1]-jump,jump)=bias[l-1];
+
+        weights[l-1].resize(nbNeurons[l],nbNeurons[l-1]);
+    }
+}
+
+bool testPoint(Eigen::VectorXd const& point, std::vector<Eigen::VectorXd>& points, double const epsClose)
+{
+    int const taille = points.size();
+    int i=0;
+
+    if (taille==0)
+    {
+        points.push_back(point);
+        return true;
+    }
+
+    while ((points[i]-point).norm()>epsClose){i++;}
+    if(i==taille)
+    {
+        points.push_back(point);
+        return true;
+    }
+    else{return false;}
+}
+
+
+
+
--- utilities.h
+++ utilities.h
@@ -1,11 +1,22 @@
 #ifndef UTILITIES
 #define UTILITIES
 
+#include <iostream>
 #include <vector>
 #include <map>
 #include <Eigen/Dense>
 
 
 int proportion(Eigen::VectorXd const& currentPoint, std::vector<Eigen::VectorXd> const& points, std::vector<double>& proportions,  std::vector<double>& distances, double const& epsNeight);
+
+double distance(std::vector<Eigen::MatrixXd> const& weightsPrec, std::vector<Eigen::VectorXd> const& biasPrec,
+std::vector<Eigen::MatrixXd> const& weights, std::vector<Eigen::VectorXd> const& bias, std::string norm="2");
+
+double cosVector(Eigen::VectorXd const& v1, Eigen::VectorXd const& v2);
+
+
+void tabToVector(std::vector<Eigen::MatrixXd>& weights, std::vector<Eigen::VectorXd> const& bias, int const& L, std::vector<int> const& nbNeurons, std::vector<int> const& globalIndices,
+Eigen::VectorXd& point);
+bool testPoint(Eigen::VectorXd const& point, std::vector<Eigen::VectorXd>& points, double const epsClose=std::pow(10,-7));
 
 #endif
