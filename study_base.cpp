5f0d2f320f1673de90bc0cff43576a96fc4a28d7
Bilel BENSAID <bilel.bensaid@etu.emse.fr>
2021-05-28 10:38:19 +0200

    correction de la fonction pour compter le nombre de minimums distincts

--- study_base.cpp
+++ study_base.cpp
@@ -6,12 +6,13 @@ double const eps, int const maxIter, double mu, double const factor, double cons
 double const pas, double const Rlim, double const factorMin, double const power, double const alphaChap, double const epsDiag,
 double const tau, double const beta, double const gamma, int const p, double const sigma, std::string const norm, double const radiusBall)
 {
-    int const N=globalIndices[L], P=data[0].cols();
+    int const N=globalIndices[2*L-1], PTrain=data[0].cols(), PTest=data[2].cols();
     unsigned seed;
     std::vector<Eigen::MatrixXd> weights(L);
     std::vector<Eigen::VectorXd> bias(L);
-    std::vector<Eigen::MatrixXd> As(L+1), slopes(L);
-    Eigen::MatrixXd E(nbNeurons[L],P);
+    std::vector<Eigen::MatrixXd> AsTest(L+1), slopesTest(L);
+    AsTest[0]=data[2];
+    Eigen::MatrixXd ETest(nbNeurons[L],PTest);
 
     std::map<std::string,double> study;
 
@@ -19,25 +20,81 @@ double const tau, double const beta, double const gamma, int const p, double con
     std::vector<Eigen::VectorXd> points;
     std::vector<double> costs;
     std::vector<double> testingErrors;
+    double testingError;
     bool newPoint;
 
     for(int i=0; i<nbTirages; i++)
     {
+        std::cout << "Tirage: " << i << std::endl;
         seed=i; initialisation(nbNeurons,weights,bias,supParameters,generator,seed);
         study = train(data[0],data[1],L,nbNeurons,globalIndices,activations,weights,bias,algo,eps,maxIter,mu,factor,RMin,RMax,b,alpha,pas,Rlim,factorMin,power,alphaChap,epsDiag,tau,beta,gamma,
         p,sigma,norm,radiusBall);
+        std::cout << "norme du gradient: " << study["finalGradient"] << std::endl;
+        std::cout << "nb d'itérations: " << study["iter"] << std::endl;
         if(study["finalGradient"]<eps)
         {
             tabToVector(weights,bias,L,nbNeurons,globalIndices,point);
             newPoint = testPoint(point,points,epsClose);
             if(newPoint)
             {
+                std::cout << "cout " << i << " : "<< study["finalCost"] << std::endl;
                 costs.push_back(study["finalCost"]);
-                fforward(data[2],data[3],L,P,nbNeurons,activations,weights,bias,As,slopes,E);
-                testingErrors.push_back(0.5*E.squaredNorm());
+                fforward(data[2],data[3],L,PTest,nbNeurons,activations,weights,bias,AsTest,slopesTest,ETest);
+                testingError = 0.5*ETest.squaredNorm();
+                testingErrors.push_back(testingError);
+                std::cout << "erreur prediction " << i << " : "<< testingError << std::endl;
+                std::cout << "" << std::endl;
             }
         }
     }
-
     std::cout << "Il y a " << points.size() << " minimums " << std::endl;
 }
+
+void denombrementMins_entropie(std::vector<Eigen::MatrixXd> const& data, int const& L, std::vector<int> const& nbNeurons, std::vector<int> const& globalIndices,
+std::vector<std::string> const& activations, std::vector<double> const& supParameters, std::string const generator, std::string algo, int const nbTirages, double const epsClose,
+double const eps, int const maxIter, double mu, double const factor, double const RMin, double const RMax, int const b, double const epsDiag, double const tau,
+double const beta, double const gamma, int const p, double const sigma)
+{
+    int const N=globalIndices[2*L-1], PTrain=data[0].cols(), PTest=data[2].cols(), nL=nbNeurons[L];
+    unsigned seed;
+    std::vector<Eigen::MatrixXd> weights(L);
+    std::vector<Eigen::VectorXd> bias(L);
+    std::vector<Eigen::MatrixXd> AsTest(L+1), slopesTest(L);
+    AsTest[0]=data[2];
+    Eigen::MatrixXd ETest(nbNeurons[L],PTest);
+
+    std::map<std::string,double> study;
+
+    Eigen::VectorXd point(N);
+    std::vector<Eigen::VectorXd> points;
+    std::vector<double> costs;
+    std::vector<double> testingErrors;
+    double testingError;
+    bool newPoint;
+
+    for(int i=0; i<nbTirages; i++)
+    {
+        std::cout << "Tirage: " << i << std::endl;
+        seed=i; initialisation(nbNeurons,weights,bias,supParameters,generator,seed);
+        study = train_entropie(data[0],data[1],L,nbNeurons,globalIndices,activations,weights,bias,algo,eps,maxIter,mu,factor,RMin,RMax,b,epsDiag,tau,beta,gamma,p,sigma);
+        std::cout << "norme du gradient: " << study["finalGradient"] << std::endl;
+        std::cout << "nb d'itérations: " << study["iter"] << std::endl;
+        if(study["finalGradient"]<eps)
+        {
+            tabToVector(weights,bias,L,nbNeurons,globalIndices,point);
+            newPoint = testPoint(point,points,epsClose);
+            if(newPoint)
+            {
+                std::cout << "cout " << i << " : "<< study["finalCost"] << std::endl;
+                costs.push_back(study["finalCost"]);
+                fforward(data[2],data[3],L,PTest,nbNeurons,activations,weights,bias,AsTest,slopesTest,ETest);
+                testingError = entropie(data[3],AsTest[L],PTest,nL);
+                testingErrors.push_back(testingError);
+                std::cout << "erreur prediction " << i << " : "<< testingError << std::endl;
+                std::cout << "" << std::endl;
+            }
+        }
+    }
+    std::cout << "Il y a " << points.size() << " minimums " << std::endl;
+}
+
--- study_base.h
+++ study_base.h
@@ -20,4 +20,9 @@ int const b=1, double const alpha=0.75, double const pas=0.1, double const Rlim=
 double const epsDiag= std::pow(10,-10), double const tau=0.1, double const beta=2, double const gamma=3, int const p=3, double const sigma=0.1, std::string const norm="2",
 double const radiusBall=std::pow(10,0));
 
+void denombrementMins_entropie(std::vector<Eigen::MatrixXd> const&  data, int const& L, std::vector<int> const& nbNeurons, std::vector<int> const& globalIndices,
+std::vector<std::string> const& activations, std::vector<double> const& supParameters, std::string const generator="uniform", std::string algo="LM", int const nbTirages=10000,
+double const epsClose=std::pow(10,-7), double const eps=std::pow(10,-7), int const maxIter=2000, double mu=10, double const factor=10, double const RMin=0.25, double const RMax=0.75, int const b=1, double const epsDiag= std::pow(10,-10), double const tau=0.1,
+double const beta=2, double const gamma=3, int const p=3, double const sigma=0.1);
+
 #endif
