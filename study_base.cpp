3cbc078ba2296103afc838f4bd8ec2bec7f817ae
Bilel BENSAID <bilel.bensaid@etu.emse.fr>
2021-06-10 12:06:06 +0200

    modifications pour prendre en compte les statégies d'ajout

--- study_base.cpp
+++ study_base.cpp
@@ -1,12 +1,14 @@
 #include "study_base.h"
 
+
 void denombrementMins(std::vector<Eigen::MatrixXd> const& data, int const& L, std::vector<int> const& nbNeurons, std::vector<int> const& globalIndices,
-std::vector<std::string> const& activations, std::vector<double> const& supParameters, std::string const generator, std::string algo, int const nbTirages, double const epsClose,
+std::vector<std::string> const& activations, std::vector<double> const& supParameters, std::string const generator, std::string algo, int const nbTirages, double const epsClose, int const nbDichotomie,
 double const eps, int const maxIter, double mu, double const factor, double const RMin, double const RMax, int const b, double const alpha,
 double const pas, double const Rlim, double const factorMin, double const power, double const alphaChap, double const epsDiag,
-double const tau, double const beta, double const gamma, int const p, double const sigma, std::string const norm, double const radiusBall)
+double const tau, double const beta, double const gamma, int const p, double const sigma, std::string const norm, double const radiusBall, int const tirageDisplay, int const tirageMin,
+std::string const strategy, double const eRelative)
 {
-    int const N=globalIndices[2*L-1], PTrain=data[0].cols(), PTest=data[2].cols();
+    int PTrain=data[0].cols(), PTest=data[2].cols();
     unsigned seed;
     std::vector<Eigen::MatrixXd> weights(L);
     std::vector<Eigen::VectorXd> bias(L);
@@ -16,85 +18,126 @@ double const tau, double const beta, double const gamma, int const p, double con
 
     std::map<std::string,double> study;
 
-    Eigen::VectorXd point(N);
-    std::vector<Eigen::VectorXd> points;
+    std::vector<std::vector<Eigen::MatrixXd>> weightsList;
+    std::vector<std::vector<Eigen::VectorXd>> biasList;
     std::vector<double> costs;
+    std::vector<double> gradientNorms;
     std::vector<double> testingErrors;
     double testingError;
-    bool newPoint;
 
-    for(int i=0; i<nbTirages; i++)
+    double meanTraining, sdTraining, meanTest, sdTest;
+    double costMinTraining, costMinTest, costMinBoth;
+    int indiceMinTraining, indiceMinTest, indiceMinBoth;
+
+    int const tirageMax=tirageMin+nbTirages;
+
+    for(int i=tirageMin; i<tirageMax; i++)
     {
-        std::cout << "Tirage: " << i << std::endl;
         seed=i; initialisation(nbNeurons,weights,bias,supParameters,generator,seed);
         study = train(data[0],data[1],L,nbNeurons,globalIndices,activations,weights,bias,algo,eps,maxIter,mu,factor,RMin,RMax,b,alpha,pas,Rlim,factorMin,power,alphaChap,epsDiag,tau,beta,gamma,
         p,sigma,norm,radiusBall);
-        std::cout << "norme du gradient: " << study["finalGradient"] << std::endl;
-        std::cout << "nb d'itérations: " << study["iter"] << std::endl;
         if(study["finalGradient"]<eps)
         {
-            tabToVector(weights,bias,L,nbNeurons,globalIndices,point);
-            newPoint = testPoint(point,points,epsClose);
-            if(newPoint)
-            {
-                std::cout << "cout " << i << " : "<< study["finalCost"] << std::endl;
-                costs.push_back(study["finalCost"]);
-                fforward(data[2],data[3],L,PTest,nbNeurons,activations,weights,bias,AsTest,slopesTest,ETest);
-                testingError = 0.5*ETest.squaredNorm();
-                testingErrors.push_back(testingError);
-                std::cout << "erreur prediction " << i << " : "<< testingError << std::endl;
-                std::cout << "" << std::endl;
-            }
+            addPoint(weights,bias,weightsList,biasList,study["finalCost"],study["finalGradient"],costs,gradientNorms,data[0],data[1],L,PTrain,nbNeurons,globalIndices,activations,epsClose,
+            nbDichotomie,eRelative,strategy);
         }
+        if (i!=0 && i%tirageDisplay==0){std::cout << "Au bout de " << i << " tirages, il y a " << weightsList.size() << " minimums" << std::endl; std::cout << " " << std::endl;}
     }
-    std::cout << "Il y a " << points.size() << " minimums " << std::endl;
+    for (size_t i=0; i<weightsList.size(); i++)
+    {
+        fforward(data[2],data[3],L,PTest,nbNeurons,activations,weightsList[i],biasList[i],AsTest,slopesTest,ETest);
+        testingError = 0.5*ETest.squaredNorm();
+        testingErrors.push_back(testingError);
+        if (i==0)
+        {
+            costMinTraining=costs[0]; indiceMinTraining=0;
+            costMinTest=testingError; indiceMinTest=0;
+            costMinBoth=(costMinTraining+testingError)/2.0; indiceMinBoth=0;
+        }
+        if (costs[i]<costMinTraining){costMinTraining=costs[i]; indiceMinTraining=i;}
+        if (testingError<costMinTest){costMinTest=testingError; indiceMinTest=i;}
+        if ((costs[i]+testingError)/2.0<costMinBoth){costMinBoth=(costs[i]+testingError)/2.0; indiceMinBoth=i;}
+
+        std::cout << "cout d'entrainement: "  << i << ": " << costs[i] << std::endl;
+        std::cout << "gradient : "  << i << ": " << gradientNorms[i] << std::endl;
+    }
+
+    meanTraining=mean(costs); sdTraining=sd(costs,meanTraining);
+    meanTest=mean(testingErrors); sdTest=sd(testingErrors,meanTest);
+
+    std::cout << "Il y a " << weightsList.size() << " minimums " << std::endl;
+
+    std::cout << "Moyenne coût d'entrainement: " << meanTraining << " +- " << sdTraining << std::endl;
+    std::cout << "Le plus petit coût d'entraînement est de: " << costMinTraining << " de numéro " << indiceMinTraining << std::endl;
+
+    std::cout << "Moyenne coût de test: " << meanTest << " +- " << sdTest << std::endl;
+    std::cout << "Le plus petit coût de test est de: " << costMinTest << " de numéro " << indiceMinTest << std::endl;
+
+    std::cout << "Le plus petit coût global est de: " << costMinBoth << " de numéro " << indiceMinBoth << std::endl;
 }
 
 void denombrementMins_entropie(std::vector<Eigen::MatrixXd> const& data, int const& L, std::vector<int> const& nbNeurons, std::vector<int> const& globalIndices,
-std::vector<std::string> const& activations, std::vector<double> const& supParameters, std::string const generator, std::string algo, int const nbTirages, double const epsClose,
+std::vector<std::string> const& activations, std::vector<double> const& supParameters, std::string const generator, std::string algo, int const nbTirages, double const epsClose, int const nbDichotomie,
 double const eps, int const maxIter, double mu, double const factor, double const RMin, double const RMax, int const b, double const epsDiag, double const tau,
-double const beta, double const gamma, int const p, double const sigma)
+double const beta, double const gamma, int const p, double const sigma, int const tirageDisplay, int const tirageMin, std::string const strategy, double const eRelative)
 {
-    int const N=globalIndices[2*L-1], PTrain=data[0].cols(), PTest=data[2].cols(), nL=nbNeurons[L];
+    int const nL=nbNeurons[L], PTrain=data[0].cols(), PTest=data[2].cols();
     unsigned seed;
     std::vector<Eigen::MatrixXd> weights(L);
     std::vector<Eigen::VectorXd> bias(L);
     std::vector<Eigen::MatrixXd> AsTest(L+1), slopesTest(L);
     AsTest[0]=data[2];
-    Eigen::MatrixXd ETest(nbNeurons[L],PTest);
+    Eigen::MatrixXd E_invTest(nbNeurons[L],PTest), E2_invTest(nbNeurons[L],PTest);
 
     std::map<std::string,double> study;
 
-    Eigen::VectorXd point(N);
-    std::vector<Eigen::VectorXd> points;
+    std::vector<std::vector<Eigen::MatrixXd>> weightsList;
+    std::vector<std::vector<Eigen::VectorXd>> biasList;
     std::vector<double> costs;
+    std::vector<double> gradientNorms;
     std::vector<double> testingErrors;
     double testingError;
-    bool newPoint;
 
-    for(int i=0; i<nbTirages; i++)
+    double meanTraining, sdTraining, meanTest, sdTest;
+    double costMinTraining=std::pow(10,3), costMinTest=std::pow(10,3), costMinBoth=std::pow(10,3);
+    int indiceMinTraining, indiceMinTest, indiceMinBoth;
+
+    int const tirageMax=tirageMin+nbTirages;
+
+    for(int i=tirageMin; i<tirageMax; i++)
     {
-        std::cout << "Tirage: " << i << std::endl;
         seed=i; initialisation(nbNeurons,weights,bias,supParameters,generator,seed);
         study = train_entropie(data[0],data[1],L,nbNeurons,globalIndices,activations,weights,bias,algo,eps,maxIter,mu,factor,RMin,RMax,b,epsDiag,tau,beta,gamma,p,sigma);
-        std::cout << "norme du gradient: " << study["finalGradient"] << std::endl;
-        std::cout << "nb d'itérations: " << study["iter"] << std::endl;
         if(study["finalGradient"]<eps)
         {
-            tabToVector(weights,bias,L,nbNeurons,globalIndices,point);
-            newPoint = testPoint(point,points,epsClose);
-            if(newPoint)
-            {
-                std::cout << "cout " << i << " : "<< study["finalCost"] << std::endl;
-                costs.push_back(study["finalCost"]);
-                fforward(data[2],data[3],L,PTest,nbNeurons,activations,weights,bias,AsTest,slopesTest,ETest);
-                testingError = entropie(data[3],AsTest[L],PTest,nL);
-                testingErrors.push_back(testingError);
-                std::cout << "erreur prediction " << i << " : "<< testingError << std::endl;
-                std::cout << "" << std::endl;
-            }
+            addPoint(weights,bias,weightsList,biasList,study["finalCost"],study["finalGradient"],costs,gradientNorms,data[0],data[1],L,PTrain,nbNeurons,globalIndices,activations,epsClose,
+            nbDichotomie,eRelative,strategy);
         }
+        if (i!=0 && i%tirageDisplay==0){std::cout << "Au bout de " << i << " tirages, il y a " << weightsList.size() << " minimums" << std::endl; std::cout << " " << std::endl;}
     }
-    std::cout << "Il y a " << points.size() << " minimums " << std::endl;
+    for (size_t i=0; i<weightsList.size(); i++)
+    {
+        fforward_entropie(data[2],data[3],L,PTest,nbNeurons,activations,weightsList[i],biasList[i],AsTest,slopesTest,E_invTest,E2_invTest);
+        testingError=entropie(data[3],AsTest[L],PTest,nL);
+        testingErrors.push_back(testingError);
+        if (costs[i]<costMinTraining){costMinTraining=costs[i]; indiceMinTraining=i;}
+        if (testingError<costMinTest){costMinTest=testingError; indiceMinTest=i;}
+        if ((costs[i]+testingError)/2.0<costMinBoth){costMinBoth=(costs[i]+testingError)/2.0; indiceMinBoth=i;}
+
+    }
+
+    meanTraining=mean(costs); sdTraining=sd(costs,meanTraining);
+    meanTest=mean(testingErrors); sdTest=sd(testingErrors,meanTest);
+
+    std::cout << "Il y a " << weightsList.size() << " minimums " << std::endl;
+
+    std::cout << "Moyenne coût d'entrainement: " << meanTraining << " +- " << sdTraining << std::endl;
+    std::cout << "Le plus petit coût d'entraînement est de: " << costMinTraining << " de numéro " << indiceMinTraining << std::endl;
+
+    std::cout << "Moyenne coût de test: " << meanTest << " +- " << sdTest << std::endl;
+    std::cout << "Le plus petit coût de test est de: " << costMinTest << " de numéro " << indiceMinTest << std::endl;
+
+    std::cout << "Le plus petit coût global est de: " << costMinBoth << " de numéro " << indiceMinBoth << std::endl;
+
 }
 
--- study_base.h
+++ study_base.h
@@ -7,22 +7,24 @@
 #include <vector>
 #include <map>
 
-#include "data.h"
 #include "init.h"
+#include "propagation.h"
 #include "training.h"
 #include "training_entropie.h"
 #include "utilities.h"
+#include "addStrategy.h"
+
 
 void denombrementMins(std::vector<Eigen::MatrixXd> const&  data, int const& L, std::vector<int> const& nbNeurons, std::vector<int> const& globalIndices,
 std::vector<std::string> const& activations, std::vector<double> const& supParameters, std::string const generator="uniform", std::string algo="LM", int const nbTirages=10000,
-double const epsClose=std::pow(10,-7), double const eps=std::pow(10,-7), int const maxIter=2000, double mu=10, double const factor=10, double const RMin=0.25, double const RMax=0.75,
+double const epsClose=std::pow(10,-7), int const nbDichotomie = std::pow(2,4), double const eps=std::pow(10,-7), int const maxIter=2000, double mu=10, double const factor=10, double const RMin=0.25, double const RMax=0.75,
 int const b=1, double const alpha=0.75, double const pas=0.1, double const Rlim=0.01, double const factorMin=std::pow(10,-3), double const power=2.0, double const alphaChap=2.0,
 double const epsDiag= std::pow(10,-10), double const tau=0.1, double const beta=2, double const gamma=3, int const p=3, double const sigma=0.1, std::string const norm="2",
-double const radiusBall=std::pow(10,0));
+double const radiusBall=std::pow(10,0), int const tirageDisplay = 50, int const tirageMin=0, std::string const strategy="CostAll", double const eRelative=0.01);
 
 void denombrementMins_entropie(std::vector<Eigen::MatrixXd> const&  data, int const& L, std::vector<int> const& nbNeurons, std::vector<int> const& globalIndices,
 std::vector<std::string> const& activations, std::vector<double> const& supParameters, std::string const generator="uniform", std::string algo="LM", int const nbTirages=10000,
-double const epsClose=std::pow(10,-7), double const eps=std::pow(10,-7), int const maxIter=2000, double mu=10, double const factor=10, double const RMin=0.25, double const RMax=0.75, int const b=1, double const epsDiag= std::pow(10,-10), double const tau=0.1,
-double const beta=2, double const gamma=3, int const p=3, double const sigma=0.1);
+double const epsClose=std::pow(10,-7), int const nbDichotomie = std::pow(2,4), double const eps=std::pow(10,-7), int const maxIter=2000, double mu=10, double const factor=10, double const RMin=0.25, double const RMax=0.75, int const b=1, double const epsDiag= std::pow(10,-10), double const tau=0.1,
+double const beta=2, double const gamma=3, int const p=3, double const sigma=0.1, int const tirageDisplay = 50, int const tirageMin=0, std::string const strategy="CostAll", double const eRelative=0.01);
 
 #endif
